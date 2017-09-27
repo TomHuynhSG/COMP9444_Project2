b5642605aaf6d96473cf71e137f1fd7e749cd1f8
payal <32151860+payaljordan@users.noreply.github.com>
2017-09-27 12:55:48 +1000

    Some work on task 2

--- stage2/hw2sent/implementation.py
+++ stage2/hw2sent/implementation.py
@@ -3,9 +3,32 @@ import numpy as np
 import glob #this will be useful when reading reviews from file
 import os
 import tarfile
-
-
+import numpy as np
 batch_size = 50
+import re
+
+
+def clean_str(string):
+    """
+    Tokenization/string cleaning for dataset
+    Every dataset is lower cased except
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)     
+    string = re.sub(r"\'s", " \'s", string) 
+    string = re.sub(r"\'ve", " \'ve", string) 
+    string = re.sub(r"n\'t", " n\'t", string) 
+    string = re.sub(r"\'re", " \'re", string) 
+    string = re.sub(r"\'d", " \'d", string) 
+    string = re.sub(r"\'ll", " \'ll", string) 
+    string = re.sub(r",", " , ", string) 
+    string = re.sub(r"!", " ! ", string) 
+    string = re.sub(r"\(", " \( ", string) 
+    string = re.sub(r"\)", " \) ", string) 
+    string = re.sub(r"\?", " \? ", string) 
+    string = re.sub(r"\s{2,}", " ", string) 
+    string = re.sub(r'<.*?>', string)
+    return string.strip().lower()
+
 
 def load_data(glove_dict):
     """
@@ -17,9 +40,9 @@ def load_data(glove_dict):
     form
     Testing
     """
-    return data
+    #return data
 
-
+print(2)
 def load_glove_embeddings():
     """
     Load the glove embeddings into a array and a dictionary with words as
@@ -29,11 +52,24 @@ def load_glove_embeddings():
             word_index_dict: a dictionary matching a word in string form to
             its index in the embeddings array. e.g. {"apple": 119"}
     """
-    #data = open("glove.6B.50d.txt",'r',encoding="utf-8")
+    #print(3)
+    data = open("glove.6B.50d.txt",'r',encoding="utf-8")
+    word_index_dict={}
+    embeddings= np.zeros(shape=(400000,50))
+    count=0
+    for line in data.readlines():
+        row=line.strip().split(' ')
+        embeddings=np.append(embeddings,np.array([row[1:]]),axis=0)
+        word_index_dict[row[0]]=count
+        count=count+1
+   
     #if you are running on the CSE machines, you can load the glove data from here
     #data = open("/home/cs9444/public_html/17s2/hw2/glove.6B.50d.txt",'r',encoding="utf-8")
-    return embeddings
-
+    
+    return embeddings,word_index_dict
+print(3) 
+embd,wrds=  load_glove_embeddings()
+print(wrds['the'])
 
 def define_graph(glove_embeddings_arr):
     """
